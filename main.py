e818cad1c93e5a5a719ea4ee8eebe3977edad722
Vincent Josse <vincent.josse@telecom-paris.fr>
2021-11-13 19:53:01 +0100

    Implemented optimize function and linear schedule on epsilon greedy action selection

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ import random
 MAX_STEPS = 500000
 
 
-def tetris_train(episode_nb=5000):
+def tetris_train(episode_nb=5000, min_steps_training=50000, target_update_frequency=30000):  # min_steps_training is considered to be smaller than the memory capacity
     env = environment.TetrisGBAEnvironment()
     tt = trainer.TetrisTrainer()
+    total_steps = 0
 
     for episode in range(episode_nb):
         state = env.reset()
@@ -17,17 +18,21 @@ def tetris_train(episode_nb=5000):
         for step in range(MAX_STEPS):
             #action = function(state)   # ToDo
             action = random.randint(0, 4)
-
             new_state, reward, done = env.step(action, state)
-            print(reward)
-            # ToDo: Learn
-            #trainer.learn(state, new_state, action, reward)
+
+            tt.memory.push(state, new_state, action, reward)
+            if tt.memory.curr_size > min_steps_training:
+                trainer.optimize()
 
             if done:
                 break
 
             state = new_state
+            total_steps += 1
+            if total_steps % target_update_frequency == 0:
+                tt.update_target()
 
+            # ToDo: Load and save weights,  pickle trainer
 
 if __name__ == "__main__":
     tetris_train()
--- trainer.py
+++ trainer.py
@@ -1,19 +1,54 @@
 import model
-
+import utils
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+import random
 
 class TetrisTrainer():
 
-    def __init__(self):
+    def __init__(self, capacity=500000, batch_size=128, gamma=0.999, lr=0.0001, epsilon_start=0.99, schedule_size=1000000):
+        self.batch_size = batch_size
+        self.gamma = gamma
+        self.learning_rate = lr
+        self.epsilon = epsilon_start
+        self.schedule_size = schedule_size
 
-        self.q_network = model.TetrisNetwork(input_dim=200, hidden_size=32, output_dim=5)
+        self.q_network = model.TetrisNetwork(input_dim=200, hidden_size=32, output_dim=5)  # The one we choose the actions from
+        self.target_q_network = model.TetrisNetwork(input_dim=200, hidden_size=32, output_dim=5)  # The one we will be optimizing from (in part)
+        self.update_target()
 
+        self.memory = utils.ReplayMemory(capacity)
+        self.optimizer = optim.Adam(self.q_network.parameters(), lr=self.learning_rate, eps=1e-5)
 
     def optimize(self):
-        pass
+        batch = self.memory.sample(batch_size=self.batch_size)
+        state, new_state, action, reward = batch[:, 0], batch[:, 1], batch[:, 2], batch[:, 3]
+        y = self.q_network(state)
+        state_action_values = torch.stack([y[i, action[i]] for i in range(self.batch_size)])
+        next_state_values = self.target_q_network(new_state).max(1)[0]
+        expected_state_action_values = reward + self.gamma * next_state_values
+        loss = F.mse_loss(state_action_values, expected_state_action_values)
 
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
-    def select_action(self):
-        pass
+    def select_action(self, state, total_steps):
+        rand = random.random()  # real number between 0 and 1 uniformly distributed
+        if rand > self.epsilon:
+            # Act greedily
+            action = torch.argmax(self.q_network(state))
+        else:
+            # Act randomly
+            action = random.randint(0, 4)
+        self.epsilon -= 1 / self.schedule_size  # ToDo: fix ugly code
+        self.epsilon = max(0.02, self.epsilon)
+        return action
+
+    def update_target(self):
+        self.target_q_network.load_state_dict(self.q_network.state_dict())
+
 
     # ToDo later:
     # - Save/checkpoint our training progess  =  saving the weights of the network to a file
