10178134c2f1f54339f171ca23bfc42145e82b86
Vincent Josse <vincent.josse@telecom-paris.fr>
2021-11-13 17:42:49 +0100

    Working state retrieval (modulo the strange living cube in the background), working performing an action in the environment, and reward function

--- environment.py
+++ environment.py
@@ -4,9 +4,13 @@ from PIL import Image
 import torchvision
 import torch
 import torch.nn.functional as F
+import utils
 
 GBA_REGION = (1, 51, 241, 211)
 GRID_REGION = (81, 51, 161, 211)
+
+
+# ToDo: Put the tensors on the GPU and don't forget to go with no_grads when necessary
 
 class TetrisGBAEnvironment(gym.Env):
     def __init__(self):
@@ -14,25 +18,78 @@ class TetrisGBAEnvironment(gym.Env):
         self.screen = d3dshot.create(capture_output="pytorch_float_gpu")  # ToDo: Try other devices for the preprocessing
         self.screen.capture(target_fps=60, region=GBA_REGION)
 
-    def step(self, action):
+    def reset(self): # Will return state
+        return torch.zeros((1, 20, 10), dtype=torch.float)
 
-        #return new_state, reward
-        pass
+    def step(self, action, state):
+        done = False
+        self.perform_action(action)
+        # wait a bit
+        new_state = self.get_observation()
+        reward = self.get_reward(state, new_state)
+        return new_state, reward, done
 
     def get_observation(self):
         grid_pixels = self.screen.screenshot(region=GRID_REGION).permute(2, 0, 1)
-        #grid_pixels = torch.tensor((grid_pixels.mean(keepdim=True, dim=0) > 0.3), dtype=torch.float)
-        #torchvision.transforms.ToPILImage()(grid_pixels).save("test_catpure2.png")
         grid_pixels = torch.tensor(grid_pixels.mean(keepdim=True, dim=0) > 0.3, dtype=torch.float)
         grid = F.avg_pool2d(grid_pixels, (8, 8)) > 0.6  #  Should output a 20x10 grid
-        print(torch.tensor(grid, dtype=torch.int))
-
+        return torch.tensor(grid, dtype=torch.float)
 
     def get_reward(self, state, new_state):
-        pass
+        height, new_height = self.compute_height(state), self.compute_height(new_state)
+
+        if new_height < height:
+            return (height - new_height)*torch.tensor(0.2, dtype=torch.float)
+        if new_height > height:
+            return new_height * torch.tensor(-0.05, dtype=torch.float)
+        else:
+            return torch.tensor(0.0, dtype=torch.float)
+
+    def compute_height(self, state):
+        height = state.sum(dim=2)
+        height = torch.argmax(torch.tensor(torch.flip(height, dims=[0,1]) == 0.0, dtype=torch.int), dim=1).item() + 1
+        return height
+
+    def perform_action(self, action):
+        if action == 0:  # DO NOTHING
+            utils.pressAndHold('ctrl')
+            for _ in range(12):
+                utils.press('n')
+            utils.release('ctrl')
+
+        elif action == 1:  # ROTATE BLOCK
+            utils.pressAndHold('x', 'ctrl')
+            for i in range(12):
+                if i == 1:
+                    utils.release('x')
+                utils.press('n')
+            utils.release('ctrl')
+
+        elif action == 2:  # LEFT
+            utils.pressAndHold('h', 'ctrl')
+            for i in range(12):
+                if i == 1:
+                    utils.release('h')
+                utils.press('n')
+            utils.release('ctrl')
+
+        elif action == 3:  # RIGHT
+            utils.pressAndHold('j', 'ctrl')
+            for i in range(12):
+                if i == 1:
+                    utils.release('j')
+                utils.press('n')
+            utils.release('ctrl')
+
+        else:  # DOWN
+            utils.pressAndHold('g', 'ctrl')
+            for i in range(12):
+                if i == 1:
+                    utils.release('g')
+                utils.press('n')
+            utils.release('ctrl')
 
 
-    def reset(self): # Will return state
 
-        pass
+
 
--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import torch
 import environment
 import trainer
 import time
+import random
 
 MAX_STEPS = 500000
-
 
 
 def tetris_train(episode_nb=5000):
@@ -14,15 +14,14 @@ def tetris_train(episode_nb=5000):
     for episode in range(episode_nb):
         state = env.reset()
 
-
         for step in range(MAX_STEPS):
-            action = function(state)   # ToDo
+            #action = function(state)   # ToDo
+            action = random.randint(0, 4)
 
-            new_state, reward, done = env.step(action)
-
-
+            new_state, reward, done = env.step(action, state)
+            print(reward)
             # ToDo: Learn
-            trainer.learn(state, new_state, action, reward)
+            #trainer.learn(state, new_state, action, reward)
 
             if done:
                 break
@@ -30,11 +29,5 @@ def tetris_train(episode_nb=5000):
             state = new_state
 
 
-
 if __name__ == "__main__":
-    #tetris_train()
-    env = environment.TetrisGBAEnvironment()
-
-    time.sleep(3)
-    env.get_observation()
-
+    tetris_train()
